f29a9089a6101e82aa56df130efc331bfada0458
NAME <mail@gmail.com>
2024-06-28 18:30:05 +0800

    add scp update features

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use anyhow::Result;
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
 use ssh2::Session;
+use std::io::Write;
+use std::path::Path;
 use std::{i32, io::Read, net::TcpStream};
 
 #[pyclass]
@@ -28,6 +29,40 @@ impl Client {
         channel.read_to_string(&mut result).unwrap();
         let exit_code = channel.exit_status().unwrap();
         Ok((result, exit_code))
+    }
+
+    fn upload_file(&mut self, content: &str, target_path: &str) -> PyResult<bool> {
+        let content_bytes = content.as_bytes();
+        let mut remote_file = self
+            .sess
+            .scp_send(
+                Path::new(target_path),
+                0o644,
+                content_bytes.len().try_into().unwrap(),
+                None,
+            )
+            .unwrap();
+        remote_file.write(content_bytes).unwrap();
+        remote_file.send_eof().unwrap();
+        remote_file.wait_eof().unwrap();
+        remote_file.close().unwrap();
+        remote_file.wait_close().unwrap();
+        Ok(true)
+    }
+
+    fn download_file(&mut self, target_path: &str) -> PyResult<String> {
+        let (mut remote_file, _) = self.sess.scp_recv(Path::new(target_path)).unwrap();
+        let mut contents = Vec::new();
+        remote_file.read_to_end(&mut contents).unwrap();
+        remote_file.send_eof().unwrap();
+        remote_file.wait_eof().unwrap();
+        remote_file.close().unwrap();
+        remote_file.wait_close().unwrap();
+
+        match String::from_utf8(contents) {
+            Ok(result) => return Ok(result),
+            _ => return Err(PyValueError::new_err("String Conversion failed.")),
+        };
     }
 }
 
